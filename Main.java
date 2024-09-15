e27e582ed75908d03e13220d8a51489dab1795a6
Marina Barykina <marinakliman9@gmail.com>
2024-09-15 14:56:30 +1000

    исправлена ошибка

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Collections;
 import java.util.Comparator;
+
 
 class УчебнаяГруппа {
     private String название;
